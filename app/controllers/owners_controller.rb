a9aaa95e06128ac87447664c3982b8c80decfb9d
Keesha Hargrove <kharg14@gmail.com>
2020-10-10 02:05:42 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -20,7 +20,7 @@ class OwnersController < ApplicationController
   
    get '/owners/:id/edit' do 
     @owner = Owner.find(params[:id])
-    @pets = Pet.all
+    @pets = Pets.all
     erb :'/owners/edit'
   end
 
